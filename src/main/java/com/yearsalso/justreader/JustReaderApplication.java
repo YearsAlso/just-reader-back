0c91f5245f98ebc2d799854b8a29f42c7cbbb3eb
1334713380@qq.com <wodetuan2012>
2025-06-29 11:08:14 +0800

    initialize project structure with Maven and Spring Boot setup

---
+++ src/main/java/com/yearsalso/justreader/JustReaderApplication.java
@@ -0,0 +1,13 @@
+package com.yearsalso.justreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yearsalso/justreader/JustReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yearsalso.justreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
